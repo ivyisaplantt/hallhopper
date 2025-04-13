11a2761bed6ef8907bc39c1650b6efde692bd64a
Emily Chen <echen583@gmail.com>
2025-04-13 01:58:33 -0400

    imported CORS

--- backend/routes.py
+++ backend/routes.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import json
 import networkx as nx
 
@@ -37,4 +38,5 @@ def route():
         }), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+    
